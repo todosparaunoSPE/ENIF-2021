6773cb368975a1c0e64d610c3bbf40b7c5091f04
todosparaunoSPE <148250432+todosparaunoSPE@users.noreply.github.com>
2023-11-08 17:48:14 -0600

    Add files via upload

---
+++ pages/42 Formas de ahorrar que tiene la población formal e informal.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 30 15:56:24 2023
+
+@author: jperezr
+"""
+
+import altair as alt
+import pandas as pd
+import streamlit as st
+
+
+from PIL import Image
+
+
+st.subheader("Formas de ahorrar que tiene la población: formal e informal")    
+#st.subheader("Diseño Estadístico para tomar una muestra y llevar a cabo la Encuesta Nacional de Inclusión Financiera 2021")    
+st.subheader("") 
+
+ 
+ 
+st.markdown('<div style="text-align: justify;">•	La población con ahorro formal aumentó un punto porcentual en el 2021 respecto del año 2018. De 20% a 21%</div>', unsafe_allow_html=True)
+st.subheader("")
+st.markdown('<div style="text-align: justify;">•	La población que ahorra de manera informal disminuyo 9 puntos porcentuales en el 2021, respecto del año 2018. 63% a 54%.</div>', unsafe_allow_html=True)
+st.subheader("")
+st.markdown('<div style="text-align: justify;">•	La población que no ahorra aumento 8 puntos porcentuales en el 2021 respecto del año 2018. De 32% a 40%</div>', unsafe_allow_html=True)
+st.subheader("")
+st.markdown('<div style="text-align: justify;">•	La población por rango de edad que más ahorra de manera formal es: 18 a 29 años. 25% de la población total.</div>', unsafe_allow_html=True)
+st.subheader("")
+st.markdown('<div style="text-align: justify;">•	La población por rango de edad que más ahorra de manera informal es: 18 a 29 años. 67% de la población total.</div>', unsafe_allow_html=True)
+st.subheader("")
+st.markdown('<div style="text-align: justify;">•	La población por rango de edad que más ahorra por nivel educativo de manera formal es: Licenciatura o más con 45% de la población total.</div>', unsafe_allow_html=True)
+st.subheader("")
+st.markdown('<div style="text-align: justify;">•	La población por rango de edad que más ahorra de manera informal por nivel educativo es: Licenciatura o más de la población total.</div>', unsafe_allow_html=True)
+st.subheader("")
+st.markdown('<div style="text-align: justify;">•	La población por rango de edad que menos ahorra de manera formal por nivel educativo es: Hasta primaria con 7% de la población total.</div>', unsafe_allow_html=True)
+st.subheader("")
+st.markdown('<div style="text-align: justify;">•	La población por rango de edad que más ahorra en cuanto aportaciones voluntarias por nivel educativo es: Licenciatura o más con 10% de la población total, aumentando 4 puntos porcentuales respecto de los años 2015 y 2018.</div>', unsafe_allow_html=True)
+st.subheader("")
+st.markdown('<div style="text-align: justify;">•	La población por rango de edad que menos ahorra en cuanto aportaciones voluntarias por nivel educativo es: Primaria con 3% de la población total, aumentando 1 punto porcentual respecto del año 2018.</div>', unsafe_allow_html=True)
+st.subheader("")
+st.markdown('<div style="text-align: justify;">•	Una de las principales razones por la que la población no realiza a portaciones voluntarias es: No le queda dinero para ahorrar con 49 % de la población total.</div>', unsafe_allow_html=True)
+st.subheader("")
+
+
+
+
+
---
+++ pages/45 Formas de ahorrar que tiene la población formal e informal.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 30 15:56:24 2023
+
+@author: jperezr
+"""
+
+import altair as alt
+import pandas as pd
+import streamlit as st
+
+
+from PIL import Image
+
+
+st.subheader("Formas de ahorrar que tiene la población: formal e informal")    
+#st.subheader("Diseño Estadístico para tomar una muestra y llevar a cabo la Encuesta Nacional de Inclusión Financiera 2021")    
+st.subheader("") 
+
+ 
+#st.title("Tipo de población y Sexo")
+ 
+st.subheader("")    
+st.subheader("")    
+
+ 
+
+image = Image.open('imagenes/42.jpg')
+
+st.image(image, caption='')
+
+
+
+  
+st.subheader("")    
+st.subheader("")    
